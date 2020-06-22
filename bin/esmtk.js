1fc32791e21b83c0dd8455c03e94313d00951808
Evan <evanplaice@gmail.com>
2020-06-22 10:29:28 -0600

    Add Preflight

--- bin/esmtk.js
+++ bin/esmtk.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import cli from 'commander';
+import { preflight } from '../src/preflight.js';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const pkg = require('../package.json');
+
+preflight();
 
 cli.version(pkg.version, '-v, --version')
 
---
+++ src/preflight.js
@@ -0,0 +1,10 @@
+export function preflight() {
+  var match = process.version.match(/v(\d+)\.(\d+)/)
+  var major = parseInt(match[1], 10)
+  var minor = parseInt(match[2], 10)
+
+  if (major === 13 && minor < 12) {
+    console.error('For Node 13.x, 13.12 or greater is required')
+    process.exit(1)
+  }
+}

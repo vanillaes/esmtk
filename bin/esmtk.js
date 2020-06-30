6f1c7d363ffa26afc01c4e8e6b71e58aeaedee95
Evan <evanplaice@gmail.com>
2020-06-29 20:19:31 -0600

    Add 'build' Command

--- bin/esmtk.js
+++ bin/esmtk.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import cli from 'commander'
 import { preflight } from '../src/preflight.js'
+import { bundle } from './commands/bundle.js'
 import { lint } from './commands/lint.js'
+
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const pkg = require('../package.json')
@@ -9,6 +11,11 @@ const pkg = require('../package.json')
 preflight()
 
 cli.version(pkg.version, '-v, --version')
+cli.command('bundle <input> <output>')
+  .description('Bundle the source using ESBuild')
+  .action((input, output) => {
+    bundle(input, output)
+  })
 cli.command('lint')
   .description('Lint the package using StandardJS')
   .option('--fix', 'Automatically fix problems')

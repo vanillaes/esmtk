294b53aadab3c4bf0b1778db0e2b7799b79e1ac7
Evan Plaice <evanplaice@gmail.com>
2026-03-04 11:35:00 -0700

    Fix 'lint --fix' Message

--- bin/commands/lint.js
+++ bin/commands/lint.js
@@ -4,10 +4,22 @@ import { join } from 'path'
 const BIN_PATH = join(process.cwd(), 'node_modules', '.bin', 'standard')
 
 export async function lint (flags) {
-  spawn(BIN_PATH, flags, {
+  const child = spawn(BIN_PATH, flags, {
     cwd: process.cwd(),
-    stdio: ['pipe', process.stdout, process.stderr]
+    stdio: ['pipe', 'pipe', 'pipe']
   }).on('error', err => {
-    console.error(err)
+    console.error(`test${err}`)
+  })
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(`${data}`)
+  })
+
+  child.stderr.on('data', (data) => {
+    if (data.toString() === 'standard: Run `standard --fix` to automatically fix some problems.\n') {
+      process.stderr.write('standard: Run `esmtk lint --fix` to automatically fix some problems.\n')
+    } else {
+      process.stderr.write(`${data}`)
+    }
   })
 }

d52663936880c218c5bd2bcca5775d3c0749f033
Evan Plaice <evanplaice@gmail.com>
2026-04-16 11:05:34 -0600

    Fix CD/JSR

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* @ts-self-types="./index.d.ts" */
 export { Package } from './package.js'
 export { PackageLock } from './packagelock.js'
 export { exists, fileExists, installed, match, readGitIgnore, readNPMIgnore, which } from './util.js'

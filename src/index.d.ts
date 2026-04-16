8d39c3eeed053bd298842ed2618b87d7446c446f
Evan Plaice <evanplaice@gmail.com>
2026-04-16 16:39:13 -0600

    v2.2.4

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export { JSR } from "./jsr.js";
+export { runScript } from "./npm.js";
+export { Package } from "./package.js";
+export { PackageLock } from "./packagelock.js";
+export { exists, fileExists, installed, match, readGitIgnore, readNPMIgnore, which } from "./util.js";

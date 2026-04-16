2e1124aa217313d5e6047a95a8f10b100aa54760
Evan Plaice <evanplaice@gmail.com>
2026-04-16 13:49:13 -0600

    Add Missing Typings

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+export { JSR } from "./jsr.js";
+export { Package } from "./package.js";
+export { PackageLock } from "./packagelock.js";
+export { exists, fileExists, installed, match, readGitIgnore, readNPMIgnore, which } from "./util.js";
